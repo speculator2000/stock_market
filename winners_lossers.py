3cb5841c42f635acd90e0eae2a1d2c74ce9df3ef
Franklin Chidi <69617851+speculator2000@users.noreply.github.com>
2022-09-06 22:37:10 -0400

    Update winners_lossers.py

--- winners_lossers.py
+++ winners_lossers.py
@@ -5,7 +5,9 @@ import yahoo_fin.stock_info as si
 df1 = si.get_day_gainers()
 df2 = si.get_day_losers()
 
-st.subheader("Top 100 Gainers Today")
+# st.subheader("Top 100 Gainers")
+st.markdown("<h2 style='text-align: center; color: Green;'>Top 100 Gainers</h2>", unsafe_allow_html=True)
+# st.header(company_name + """ Analysis""")
 
 st.write(df1.style.format({'Price (Intraday)': '${:,.2f}',
                            'Change': '${:,.2f}',
@@ -15,7 +17,8 @@ st.write(df1.style.format({'Price (Intraday)': '${:,.2f}',
                            'Market Cap': '${:,.0f}',
                            'PE Ratio (TTM)': '{:,.2f}'}))
 st.text('')
-st.subheader("Bottom 100 Stocks Today")
+# st.subheader("Bottom 100 Stocks")
+st.markdown("<h2 style='text-align: center; color: Red;'>Bottom 100 Stocks</h2>", unsafe_allow_html=True)
 st.write(df2.style.format({'Price (Intraday)': '${:,.2f}',
                            'Change': '${:,.2f}',
                            '% Change': '{:,.1f}%',
